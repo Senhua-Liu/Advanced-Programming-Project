7c1c88e56e4b59ad6503e5ed40a79d15dbbe40e8
alenn <alennpham@gmail.com>
2024-01-11 18:01:22 +0100

    update

---
+++ frontend/src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+      <div className="header">
+        <h1>Your Page Title</h1>
+        <p>Subtitle or additional information</p>
+      </div>
+    );
+  };
+  
+  export default Header;
+
+  
---
+++ frontend/src/Title.js
@@ -0,0 +1,19 @@
+// src/Title.js
+
+import React from 'react'
+import './Title.css';
+
+
+function Title() {
+    return (
+      <div className="Title">
+        <h1>Homepage</h1>
+        <button alt="buttonhdr1">Upload Files</button>
+        <button>Meeting</button>
+        <button>Evaluation Form</button>
+        <button>Report Validation</button>
+      </div>
+    )
+  }
+  
+  export default Title
