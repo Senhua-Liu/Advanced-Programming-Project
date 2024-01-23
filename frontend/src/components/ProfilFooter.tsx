32e88458b4776ab964ba030477db61502f2ff645
FENG <38035703+HuitingFENG@users.noreply.github.com>
2024-01-23 09:44:49 +0100

    update

--- frontend/src/components/ProfilFooter.tsx
+++ frontend/src/components/ProfilFooter.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Box,Flex,Link,Text,Image,Button,Stack,Center,Icon } from "@chakra-ui/react";
 import {Link as RouterLink, BrowserRouter as Router, Routes, Route, } from "react-router-dom";
-import { FaQuestionCircle, FaBook, FaCog, FaUser, FaMobileAlt, FaPhone, FaComment, FaEnvelope, FaRegCommentDots } from "react-icons/fa";
+import { FaQuestionCircle, FaBook, FaCog, FaUser, FaMobileAlt, FaPhone, FaComment, FaEnvelope, FaRegCommentDots, FaUserPlus, FaRedo } from "react-icons/fa";
 import { AiOutlineCopyright, AiOutlineMail } from "react-icons/ai";
 import { FiLink } from "react-icons/fi";
 import { GiEarthAmerica } from "react-icons/gi";
@@ -9,10 +9,10 @@ import { GiEarthAmerica } from "react-icons/gi";
 const ProfilFooter = () => {
 
   return (
-    <Flex p={10} align="center" justify="flex-end" bg="#dddddd" as="footer" position="fixed" bottom="0" width="100%" left="0" height="200px" >
-        <Flex align="center" justify="flex-end" gap={10} p={1}>
-            <Button bgColor="#0C2340" ><Text fontWeight="bold"  fontSize="2xl" color="white">Register / Reset</Text></Button>
-        </Flex>
+    <Flex p={10} flexDir="row" align="center" justify="flex-end" bg="#dddddd" as="footer" position="fixed" bottom="0" width="100%" left="0" height="200px" >
+        {/* <Flex align="center" justify="flex-end" gap={10} p={1}> */}
+        <Button bgColor="#0C2340" gap={3}  p={8} width="300px" ><FaUserPlus color="white" /><Text fontWeight="bold"  fontSize="2xl" color="white">Register / Reset</Text><FaRedo color="white" /></Button>
+        {/* </Flex> */}
     </Flex>
 
   );
